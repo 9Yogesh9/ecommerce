167650366231bbe26fbef86251531e6b0bada8c2
Yogesh Bhat <yogesh99bhat@gmail.com>
2023-03-06 01:59:33 +0530

    Initial data loaded for mobiles

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CartItems from "../components/CartItems";
 
 import '../index.css';
 
 const Content = () => {
+
+    useEffect(() => {
+        fetch("https://my-json-server.typicode.com/9Yogesh9/ecommerce/cart")
+        .then((response) => response.json())
+        .then((data) => console.log(data));      
+    }, [])
+    
+
+    // "https://my-json-server.typicode.com/9Yogesh9/ecommerce/cart" to get the list of items
     return (
         <div className="content_container">
             <div className="sub_container">
